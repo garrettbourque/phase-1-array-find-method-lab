57077bca2193004c596667cdde9c3590b1d2b03d
garrettBourque <bourque.garrett@gmail.com>
2021-05-18 17:39:23 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 
+function superbowlWin(wins){
+    let outcome=wins.find(({result})=>result==='W');
+    console.log(outcome);
+    if(outcome!=undefined)
+        return outcome.year;
+
+    return outcome;
+
+}
